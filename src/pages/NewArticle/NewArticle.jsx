b1ebbadfd4b413d48fcaf921daf96df7b9aeaae7
tymob0 <87370293+tymob0@users.noreply.github.com>
2023-03-24 17:23:24 +0100

    added button and tittle field to the new article page

--- src/pages/NewArticle/NewArticle.jsx
+++ src/pages/NewArticle/NewArticle.jsx
@@ -1,12 +1,16 @@
-
-
 import React from "react";
 import RichTextEditor from '../../components/RichTextEditor/RichTextEditor';
+import Button from '@mui/material/Button';
+import Input from '@mui/material/Input';
+
+const ariaLabel = { 'aria-label': 'description' };
 
 export default function NewArticle(){
     return(
         <div>
+            <Input defaultValue="New article" inputProps={ariaLabel} style={{ fontSize: '24px' }}/>
             <RichTextEditor/>
+            <Button variant="contained">Save</Button>
         </div>
     )
 }
