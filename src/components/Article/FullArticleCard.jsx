837ced1bdce7cb4891aeacb27c12be1a7ea6feb0
tymob0 <87370293+tymob0@users.noreply.github.com>
2023-05-23 11:43:50 +0200

    added article likes

--- src/components/Article/FullArticleCard.jsx
+++ src/components/Article/FullArticleCard.jsx
@@ -1,61 +1,84 @@
 import * as React from "react";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
 import Avatar from "@mui/material/Avatar";
 import IconButton from "@mui/material/IconButton";
 import { red } from "@mui/material/colors";
 import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import ShareIcon from "@mui/icons-material/Share";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import ArticleContent from "./ArticleContent";
+import { LikeArticle } from "../../service/ArticleService";
 
 export default function FullArticleCard(props) {
+  const [likedByUser, setLikedByUser] = React.useState(false);
+
+  React.useEffect(() => {
+    if (props.article) {
+      setLikedByUser(props.article.likedByUser);
+    }
+  }, [props.article]);
+
+  const likeArticle = async () => {
+    await LikeArticle(props.article.id, props.user);
+    setLikedByUser(true);
+  };
+
+  const unlikeArticle = async () => {
+    await LikeArticle(props.article.id, props.user);
+    setLikedByUser(false);
+  };
+
+  const data = props.article;
   return (
-    <Card
-      sx={{
-        height: "100%",
-        display: "flex",
-        flexDirection: "column",
-        border: "1px solid lightgrey",
-      }}
-    >
-      <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            R
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title={props.article.title}
-        subheader={props.article.date}
-      />
-      {/* <CardMedia
-        component="img"
-        height="300"
-        image="https://howtodrawforkids.com/wp-content/uploads/2022/07/how-to-draw-an-open-book.jpg"
-        alt="Paella dish"
-        sx={{ objectFit: "cover" }}
-      /> */}
-      <CardContent sx={{ flexGrow: 1 }}>
-        {/* <Typography variant="body2" color="text.secondary"> */}
-          <ArticleContent htmlContent={props.article.text}/>
-        {/* </Typography> */}
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-      </CardActions>
-    </Card>
+    <>
+      {props.article && (
+        <Card
+          sx={{
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+            border: "1px solid lightgrey",
+          }}
+        >
+          {console.log(likedByUser)}
+          <CardHeader
+            avatar={
+              <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                R
+              </Avatar>
+            }
+            action={
+              <IconButton aria-label="settings">
+                <MoreVertIcon />
+              </IconButton>
+            }
+            title={props.article.title}
+            subheader={props.article.date}
+          />
+          <CardContent sx={{ flexGrow: 1 }}>
+            <ArticleContent htmlContent={props.article.text} />
+          </CardContent>
+          <CardActions disableSpacing>
+            <IconButton
+              onClick={likedByUser ? unlikeArticle : likeArticle}
+              aria-label="add to favorites"
+            >
+              {likedByUser ? (
+                <FavoriteIcon color="error" />
+              ) : (
+                <FavoriteBorderIcon />
+              )}
+            </IconButton>
+            <IconButton aria-label="share">
+              <ShareIcon />
+            </IconButton>
+          </CardActions>
+        </Card>
+      )}
+    </>
   );
 }
